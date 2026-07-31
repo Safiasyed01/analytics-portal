d54264f2d5b20209d2a46c22fa5d27c71f7c664e
Safiasyed01 <safiasyed09@gmail.com>
2026-08-01 01:14:05 +0500

    theme

---
+++ src/data/Mockdata.js
@@ -0,0 +1,9 @@
+export const users = [
+     { id: "USR-1001", name: "Ali", country: "Pakistan", orders: 42, revenue: 4300, status: "Active" },
+  { id: "USR-1002", name: "Sara", country: "USA", orders: 28, revenue: 6200, status: "Active" },
+  { id: "USR-1003", name: "John", country: "Canada", orders: 15, revenue: 1900, status: "Inactive" }
+];
+
+
+export const countries = ["Pakistan", "USA", "Canada"];
+export const statuses = ["Active", "Inactive"];
